a273fbfb59f9e53d42f75a0cae41bbaed806c21f
Chizzy David <davidchizindu@gmail.com>
2019-10-08 17:00:49 +0100

    fixing broken css on heroku

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
-import '../stylesheets/application.scss';
 import App from "../src/App";
 
 document.addEventListener("DOMContentLoaded", () => {
--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import './stylesheets/application.scss';
 
 export default () => (
   <Router>
